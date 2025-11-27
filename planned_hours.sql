6dad441a0ae292dca603f545b83b8c34376b5600
alimsmiley <138820344+alimsmiley@users.noreply.github.com>
2025-11-27 15:45:12 +0100

    updated planned hours, problmes fixed

--- planned_hours.sql
+++ planned_hours.sql
@@ -25,11 +25,14 @@ WITH calculated_hours AS (
 
         SUM(CASE WHEN pa.activity_name = 'Administration'
             THEN pa.planned_hours END) *
-            (SELECT factor FROM teaching_activity ta WHERE ta.activity_name = 'Administration') AS "Admin"
+            (SELECT factor FROM teaching_activity ta WHERE ta.activity_name = 'Administration') AS "Admin",
+		SUM(CASE WHEN pa.activity_name = 'Examination'
+            THEN pa.planned_hours END) *
+            (SELECT factor FROM teaching_activity ta WHERE ta.activity_name = 'Examination') AS "Exam"	
         
-    FROM course_layout cl
-    JOIN course_instance ci ON ci.instance_id = cl.course_layout_id
-    JOIN course_credits cc ON cc.course_layout_id = cl.course_layout_id
+    FROM course_instance AS ci 
+	JOIN (SELECT DISTINCT course_code, course_layout_id FROM course_layout ) AS cl ON ci.course_layout_id = cl.course_layout_id
+    JOIN course_credits cc ON cc.course_layout_id = ci.course_layout_id AND cc.study_period = ci.study_period
     LEFT JOIN planned_activity pa ON pa.instance_id = ci.instance_id
     LEFT JOIN teaching_activity ta ON ta.activity_name = pa.activity_name
     WHERE EXTRACT(YEAR FROM ci.start_date) = EXTRACT(YEAR FROM CURRENT_DATE)
@@ -46,6 +49,10 @@ SELECT
     "Lab Hours",
     "Seminar Hours",
     "Admin",
-    ("Lecture Hours" + "Tutorial Hours" + "Lab Hours" + "Seminar Hours" + "Admin") AS "Total Calculated Hours"
+	"Exam",
+    COALESCE("Lecture Hours", 0) + COALESCE("Tutorial Hours", 0) + COALESCE("Lab Hours", 0)
+	+ COALESCE("Seminar Hours", 0)
+	+ COALESCE("Admin", 0)
+	 AS "Total Calculated Hours"
 FROM calculated_hours
 ORDER BY "Course Code";
