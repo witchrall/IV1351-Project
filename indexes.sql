3217a25783a1cd2f0a6c21be4ff42b72feb7af9d
alimsmiley <138820344+alimsmiley@users.noreply.github.com>
2025-12-01 10:53:28 +0100

    explain_analyse result for all views before creating indexes and materialized view, the indexes created are under indexes.sql

---
+++ indexes.sql
@@ -0,0 +1,17 @@
+--the indexes create during SEM2
+
+CREATE INDEX instance_id_index
+ON activity_allocation(isntance_id);
+
+CREATE INDEX employment_id_index
+ON activity_allocation(employment_id);
+
+CREATE INDEX activity_name_index
+ON planned_activity(activity_name);
+
+
+CREATE INDEX person_id_index
+ON employee(person_id);
+
+CREATE INDEX course_layout_id_index
+ON course_instance(course_layout_id);
