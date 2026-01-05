ec0a649dc4aa095d02f70cee20f1f979f0baa788
alimsmiley <138820344+alimsmiley@users.noreply.github.com>
2026-01-05 17:59:05 +0100

    View2 fixed, now grouped correclty

--- SEM2.sql
+++ SEM2.sql
@@ -55,7 +55,7 @@ CREATE VIEW planned_hours AS
              JOIN course_credits cc ON cc.course_layout_id = ci.course_layout_id AND cc.study_period = ci.study_period
              LEFT JOIN planned_activity pa ON pa.instance_id = ci.instance_id
              LEFT JOIN teaching_activity ta ON ta.activity_name::text = pa.activity_name::text
-          WHERE EXTRACT(year FROM ci.start_date) = EXTRACT(year FROM CURRENT_DATE)
+          WHERE EXTRACT(year FROM ci.start_date) = 2025
           GROUP BY cl.course_code, cc.study_period, ci.instance_id, cc.hp
         )
 SELECT 
@@ -77,73 +77,119 @@ ORDER BY "Course Code";
 
 
 /* SEM2 view2 */
- CREATE VIEW allocated_hours AS
- WITH allocated_per_teacher AS (
-         SELECT cl.course_code AS "Course Code",
-            ci.instance_id AS "Course Instance ID",
-            cc.hp AS "HP",
-            concat(per.first_name, ' ', per.last_name) AS "Teacher's Name",
-                CASE
-                    WHEN pa.activity_name::text = 'Lecture'::text THEN pa.planned_hours
-                    ELSE 0
-                END * (( SELECT ta.factor
-                   FROM teaching_activity ta
-                  WHERE ta.activity_name::text = 'Lecture'::text)) AS "Lecture Hours",
-                CASE
-                    WHEN pa.activity_name::text = 'Tutorial'::text THEN pa.planned_hours
-                    ELSE 0
-                END * (( SELECT ta.factor
-                   FROM teaching_activity ta
-                  WHERE ta.activity_name::text = 'Tutorial'::text)) AS "Tutorial Hours",
-                CASE
-                    WHEN pa.activity_name::text = 'Lab'::text THEN pa.planned_hours
-                    ELSE 0
-                END * (( SELECT ta.factor
-                   FROM teaching_activity ta
-                  WHERE ta.activity_name::text = 'Lab'::text)) AS "Lab Hours",
-                CASE
-                    WHEN pa.activity_name::text = 'Seminar'::text THEN pa.planned_hours
-                    ELSE 0
-                END * (( SELECT ta.factor
-                   FROM teaching_activity ta
-                  WHERE ta.activity_name::text = 'Seminar'::text)) AS "Seminar Hours",
-                CASE
-                    WHEN pa.activity_name::text = 'Administration'::text THEN pa.planned_hours
-                    ELSE 0
-                END * (( SELECT ta.factor
-                   FROM teaching_activity ta
-                  WHERE ta.activity_name::text = 'Administration'::text)) AS "Admin",
-                CASE
-                    WHEN pa.activity_name::text = 'Examination'::text THEN pa.planned_hours
-                    ELSE 0
-                END * (( SELECT ta.factor
-                   FROM teaching_activity ta
-                  WHERE ta.activity_name::text = 'Examination'::text)) AS "Exam"
-           FROM course_instance ci
-             JOIN ( SELECT course_layout.course_code,
-                    course_layout.course_layout_id
-                   FROM course_layout) cl ON ci.course_layout_id = cl.course_layout_id
-             JOIN course_credits cc ON cc.course_layout_id = ci.course_layout_id AND cc.study_period = ci.study_period
-             JOIN planned_activity pa ON ci.instance_id = pa.instance_id
-             JOIN activity_allocation aa ON aa.instance_id = ci.instance_id
-             JOIN employee emp ON aa.employment_id = emp.employment_id
-             JOIN person per ON emp.person_id = per.person_id
-          WHERE EXTRACT(year FROM ci.start_date) = EXTRACT(year FROM CURRENT_DATE) /* as we are now in 2026 now the right side should be 2025 instead of EXTRACT(year FROM CURRENT_DATE) to see the results, as there are no data in our database for 2026*/
-        )
-SELECT 
+CREATE OR REPLACE VIEW allocated_hours_final AS
+WITH allocated_per_teacher AS (
+    SELECT
+        cl.course_code AS "Course Code",
+        ci.instance_id AS "Course Instance ID",
+        cc.hp AS "HP",
+        concat(per.first_name, ' ', per.last_name) AS "Teacher's Name",
+        SUM(
+            CASE
+                WHEN pa.activity_name::text = 'Lecture'::text THEN pa.planned_hours
+                ELSE 0::numeric
+            END
+        ) * (
+            SELECT ta_1.factor
+            FROM teaching_activity ta_1
+            WHERE ta_1.activity_name::text = 'Lecture'::text
+        ) AS "Lecture Hours",
+        SUM(
+            CASE
+                WHEN pa.activity_name::text = 'Tutorial'::text THEN pa.planned_hours
+                ELSE 0::numeric
+            END
+        ) * (
+            SELECT ta_1.factor
+            FROM teaching_activity ta_1
+            WHERE ta_1.activity_name::text = 'Tutorial'::text
+        ) AS "Tutorial Hours",
+        SUM(
+            CASE
+                WHEN pa.activity_name::text = 'Lab'::text THEN pa.planned_hours
+                ELSE 0::numeric
+            END
+        ) * (
+            SELECT ta_1.factor
+            FROM teaching_activity ta_1
+            WHERE ta_1.activity_name::text = 'Lab'::text
+        ) AS "Lab Hours",
+        SUM(
+            CASE
+                WHEN pa.activity_name::text = 'Seminar'::text THEN pa.planned_hours
+                ELSE 0::numeric
+            END
+        ) * (
+            SELECT ta_1.factor
+            FROM teaching_activity ta_1
+            WHERE ta_1.activity_name::text = 'Seminar'::text
+        ) AS "Seminar Hours",
+        SUM(
+            CASE
+                WHEN pa.activity_name::text = 'Administration'::text THEN pa.planned_hours
+                ELSE 0::numeric
+            END
+        ) * (
+            SELECT ta_1.factor
+            FROM teaching_activity ta_1
+            WHERE ta_1.activity_name::text = 'Administration'::text
+        ) AS "Admin",
+        SUM(
+            CASE
+                WHEN pa.activity_name::text = 'Examination'::text THEN pa.planned_hours
+                ELSE 0::numeric
+            END
+        ) * (
+            SELECT ta_1.factor
+            FROM teaching_activity ta_1
+            WHERE ta_1.activity_name::text = 'Examination'::text
+        ) AS "Exam"
+    FROM course_instance ci
+    JOIN (
+        SELECT DISTINCT
+            course_layout.course_code,
+            course_layout.course_layout_id
+        FROM course_layout
+    ) cl ON ci.course_layout_id = cl.course_layout_id
+    JOIN course_credits cc
+        ON cc.course_layout_id = ci.course_layout_id
+       AND cc.study_period = ci.study_period
+    JOIN planned_activity pa
+        ON ci.instance_id = pa.instance_id
+    JOIN activity_allocation aa
+        ON aa.instance_id = ci.instance_id
+ 
+    JOIN employee emp
+        ON aa.employment_id = emp.employment_id
+    JOIN person per
+        ON emp.person_id = per.person_id
+    WHERE EXTRACT(year FROM ci.start_date) = 2025
+    GROUP BY
+        cl.course_code,
+        ci.instance_id,
+        cc.hp,
+        concat(per.first_name, ' ', per.last_name)
+)
+SELECT
     "Course Code",
     "Course Instance ID",
     "HP",
     "Teacher's Name",
-    "Tutorial Hours",
     "Lecture Hours",
+    "Tutorial Hours",
     "Lab Hours",
     "Seminar Hours",
     "Admin",
     "Exam",
-    ("Lecture Hours" + "Tutorial Hours" + "Lab Hours" + "Seminar Hours" + "Admin" + "Exam") AS "Total Calculated Hours"
+    ("Lecture Hours"
+     + "Tutorial Hours"
+     + "Lab Hours"
+     + "Seminar Hours"
+     + "Admin"
+     + "Exam") AS "Total Calculated Hours"
 FROM allocated_per_teacher
-ORDER BY "Course Code";
+ORDER BY "Course Code", "Teacher's Name";
+
 
 
 /*SEM2 view3 */
